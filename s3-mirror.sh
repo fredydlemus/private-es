d679266fb1f75201f8fc5570f45865849612a1bb
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2025-09-16 21:57:26 -0600

    update screen mirroring

--- s3-mirror.sh
+++ s3-mirror.sh
@@ -1,2 +1,2 @@
-curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-05-08/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.4/2025-08-20/bin/linux/amd64/kubectl
 aws s3 cp kubectl s3://${bucket}/${prefix}/kubectl --region ${region}
